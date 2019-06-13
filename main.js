241d2a25e1ea71ec81f9ddc4dd06b79f8700e704
robson.hermes <robson.hermes@pinguin.ag>
2019-06-13 14:10:53 +0200

    small fixes

--- main.js
+++ main.js
@@ -8,9 +8,6 @@ function createWindow() {
   win.webContents.openDevTools()
   win.loadFile('test.html')
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null
   })
 }
